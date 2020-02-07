be4dc9386d8b3ddf32e54a9f9566ea4a9f48de3e
Andrei N <andreic.nica@gmail.com>
2020-02-07 20:30:14 +0200

    added Adaptive Instance Normalization

--- basic_layers.py
+++ basic_layers.py
@@ -1,4 +1,5 @@
 import torch
+import torch.
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -11,6 +12,33 @@ class IdentityNorm(nn.Module):
 
     def forward(self, input):
         return input
+
+class AdaINNorm(nn.Module):
+    def __init__(self, num_features, eps=1e-5, momentum=0.1):
+        super(AdaINNorm, self).__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+        self.weight = None
+        self.bias = None
+        self.register_buffer('running_mean', torch.zeros(num_features))
+        self.register_buffer('running_var', torch.ones(num_features))
+
+    def forward(self, x):
+        b, c = x.size(0), x.size(1)
+        running_mean = self.running_mean.repeat(b)
+        running_var  = self.running_var.repeat(c)
+
+        x_reshaped = x.contiguos().view(1, b * c, *x.size()[2:])
+
+        out = F.batch_norm(x_reshaped, running_mean, running_var, self.weight, self.bias, True,
+                           self.momentum, self.eps)
+
+        return out.view(b, c *x.size()[2:])
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' + str(self.num_features) + ')'
+
 
 
 class InstanceNorm(nn.Module):
